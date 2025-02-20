c041144d207c8fbbbab437569001875d1256093e
Amanda Bienz <bienz@unm.edu>
2025-02-20 13:00:04 -0700

    updating tests

--- tests/test_tlb.cpp
+++ tests/test_tlb.cpp
@@ -23,12 +23,16 @@ TEST(TLBTest, TestsIntests)
     table->add_page(64, 256, 0, 1);
     table->add_page(65, 312, 0, 1);
     table->add_page(66, 1024, 0, 1);
+    table->add_page(67, 0, 1, 1);
+    table->add_page(68, 120, 1, 1);
 
 
     TLB* tlb = new TLB(4,4);
     tlb->add_entry(0, 16, table->entries[64]);
     tlb->add_entry(1, 16, table->entries[65]);
     tlb->add_entry(2, 16, table->entries[66]);
+    tlb->add_entry(3, 16, table->entries[67]);
+
 
     PFN = TLB_lookup(tlb, 64);
     ASSERT_EQ(PFN, 256);
@@ -63,7 +67,7 @@ TEST(TLBTest, TestsIntests)
     }
     ASSERT_EQ(exception, true);
 
-    tlb->add_entry(0, 17, 120, 1);
+    tlb->add_entry(3, 16, table->entries[68]);
     try
     {
         exception = false;
