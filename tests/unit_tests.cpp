cfc4e4b858d7a78a3126ab79a5134e0dddd194f1
Amanda Bienz <bienz@unm.edu>
2026-01-20 14:00:11 -0700

    Updated unit test to use standard ctest

--- tests/unit_tests.cpp
+++ tests/unit_tests.cpp
@@ -1,22 +1,11 @@
-// EXPECT_EQ and ASSERT_EQ are macros
-// EXPECT_EQ test execution and continues even if there is a failure
-// ASSERT_EQ test execution and aborts if there is a failure
-// The ASSERT_* variants abort the program execution if an assertion fails 
-// while EXPECT_* variants continue with the run.
-
-#include "gtest/gtest.h"
 #include "src.hpp"
 
+## Test file checks correctness
 int main(int argc, char** argv)
 {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-
-}
-
-TEST(TLBTest, TestsIntests)
-{
-    ASSERT_EQ(return1(), 1);
-    ASSERT_GT(return1(), return0());
-    ASSERT_LT(return0(), return1());
+    // If value returned from method does not equal 0, fail test
+    if (return0() != 0)
+    {
+        fprintf(stderr, "Return value does not equal 1\n");
+    }
 }
